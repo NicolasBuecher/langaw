02cd12b4cc17d4659237b4b3ae7ba10b7dbe294a
Nicolas <buecher.nicolas@gmail.com>
2019-09-27 19:50:14 +0200

    feat: Make flies fly

--- lib/components/fly.dart
+++ lib/components/fly.dart
@@ -10,8 +10,19 @@ class Fly {
   Rect flyRect;
   bool isDead = false;
   bool isOffscreen = false;
+  Offset targetLocation;
 
-  Fly(this.game);
+  double get speed => game.tileSize * 3;
+
+  Fly(this.game) {
+    setTargetLocation();
+  }
+
+  void setTargetLocation() {
+    double x = game.rnd.nextDouble() * (game.screenSize.width - (game.tileSize * 2.025));
+    double y = game.rnd.nextDouble() * (game.screenSize.height - (game.tileSize * 2.025));
+    targetLocation = Offset(x, y);
+  }
 
   void render(Canvas c) {
     if (isDead) {
@@ -31,6 +42,16 @@ class Fly {
       flyingSpriteIndex += 30 * t;
       if (flyingSpriteIndex >= 2) {
         flyingSpriteIndex -= 2;
+      }
+
+      double stepDistance = speed * t;
+      Offset toTarget = targetLocation - Offset(flyRect.left, flyRect.top);
+      if (stepDistance < toTarget.distance) {
+        Offset stepToTarget = Offset.fromDirection(toTarget.direction, stepDistance);
+        flyRect = flyRect.shift(stepToTarget);
+      } else {
+        flyRect = flyRect.shift(toTarget);
+        setTargetLocation();
       }
     }
   }
